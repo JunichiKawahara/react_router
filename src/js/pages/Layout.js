57899ba0b273d7e44174053a2d4a086258fc5036
Kawahara Junichi <kawahara-j@IMAC5-6.local>
2020-03-03 13:45:53 +0900

    navigate

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { Link, withRouter } from "react-router-dom";
 
-export default class Layout extends React.Component {
+class Layout extends React.Component {
+  navigate() {
+    console.log(this.props.history);
+    this.props.history.push("/");
+  }
   render() {
     return (
+      <>
       <h1>KillerNews.net</h1>
+      {this.props.children}
+      <Link to="archives"><button class="btn btn-danger">archives</button></Link>
+      <Link to="settings"><button class="btn btn-success">settings</button></Link>
+      <button class="btn btn-info" onClick={this.navigate.bind(this)}>featured</button>
+      </>
     );
   }
 }
 
+export default withRouter(Layout);
